39640afe7a10e0bb802d9940cb96705e2e1bee70
Santino Scampone Garcia <santinoscampone1@gmail.com>
2025-10-01 21:53:00 -0300

    Probando IA en el front

--- Frontend/src/pages/IApage.jsx
+++ Frontend/src/pages/IApage.jsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
+import {Box, Stack, Typography} from '@mui/material';
 
 function IaModel() {
     return(
-        <h1>Pagina de IA</h1>
+        <Stack flexDirection='column' sx={{
+            minHeight: '100dvh',
+            width: '100%'
+        }}>
+            <Typography variant="h2">Pantalla de pruba de IA</Typography>
+            <Box>
+                
+            </Box>
+        </Stack>
     )
 };
 
